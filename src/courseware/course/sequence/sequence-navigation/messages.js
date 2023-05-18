5a6d7f98f336804875ba83e4451be1aac13a20f4
crtmneric <s3rkanodemir@gmail.com>
2023-05-18 10:27:20 +0300

    Changes styles

--- src/courseware/course/sequence/sequence-navigation/messages.js
+++ src/courseware/course/sequence/sequence-navigation/messages.js
@@ -3,17 +3,17 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   nextButton: {
     id: 'learn.sequence.navigation.next.button',
-    defaultMessage: 'Next',
+    defaultMessage: 'Ileri',
     description: 'Button to advance to the next section',
   },
   nextUpButton: {
     id: 'learn.sequence.navigation.next.up.button',
-    defaultMessage: 'Next Up: {title}',
+    defaultMessage: 'Bir Sonraki: {title}',
     description: 'Button to advance to the next section, with title',
   },
   previousButton: {
     id: 'learn.sequence.navigation.previous.button',
-    defaultMessage: 'Previous',
+    defaultMessage: 'Geri',
     description: 'Button to return to the previous section',
   },
 });
