824f05db82f7263ab8a5b00129fd133385bc2719
crtmneric <s3rkanodemir@gmail.com>
2023-08-15 13:29:12 +0300

    Changes styles

--- src/tab-page/ModifiedFooter.jsx
+++ src/tab-page/ModifiedFooter.jsx
@@ -13,29 +13,29 @@ const replaceHrefFooter = () => {
     footerDivs[0].innerHTML = `
     <div style="width: 100%; padding: 40px 24px !important;">
       <div style="max-width: 1336px; margin: 0 auto;">
-        <div style="display: flex; justify-content: space-between;">
-            <div><img src="https://test.pupilica.com/images/footer-logo.png" alt="Pupilica Footer" /></div>
+        <div class="footer-elements">
+            <div><img src="https://pupilica.com/images/footer-logo.png" alt="Pupilica Footer" /></div>
             <div>
                 <ul>
-                    <li><a href="https://test.pupilica.com/about-us">Hakkımızda</a></li>
-                    <li><a href="https://test.pupilica.com/educations">Eğitimler</a></li>
-                    <li><a href="https://test.pupilica.com/instructors">Eğitmenler</a></li>
+                    <li><a href="https://pupilica.com/about-us">Hakkımızda</a></li>
+                    <li><a href="https://pupilica.com/educations">Eğitimler</a></li>
+                    <li><a href="https://pupilica.com/instructors">Eğitmenler</a></li>
                 </ul>
             </div>
             <div>
                 <ul>
-                    <li><a href="https://test.pupilica.com/conditions">Kullanıcı S&ouml;zleşmesi</a></li>
-                    <li><a href="https://test.pupilica.com/faqs">Sık&ccedil;a Sorulan Sorular</a></li>
+                    <li><a href="https://pupilica.com/conditions">Kullanıcı S&ouml;zleşmesi</a></li>
+                    <li><a href="https://pupilica.com/faqs">Sık&ccedil;a Sorulan Sorular</a></li>
                     <li><a href="https://softtech.com.tr/web-sitesi-gizlilik-politikasi/" target="_blank">Gizlilik
                             S&ouml;zleşmesi</a></li>
                     <li><a href="https://softtech.com.tr/wp-content/uploads/2018/07/KVK-Ba%C5%9Fvuru-Formu.pdf"
                             target="_blank">KVKK Başvuru Formu</a></li>
-                    <li><a href="https://test.pupilica.com/contact">Bize Ulaşın</a></li>
+                    <li><a href="https://pupilica.com/contact">Bize Ulaşın</a></li>
                 </ul>
             </div>
-            <div><img src="https://test.pupilica.com/images/footer-buttons.png" alt="Pupilica" /></div>
+            <div><img src="https://pupilica.com/images/footer-buttons.png" alt="Pupilica" /></div>
         </div>
-        <div style="display: flex; align-items: center; justify-content: flex-end;padding-block: 24px !important;">  
+        <div style="footer-copyright">  
           <div style="color: rgb(255, 255, 255); font-size: 13px;">Tüm Hakları Saklıdır <span style="color: rgb(189, 225, 41)">Pupilica 2023 </span></div>
         </div>
       </div>
@@ -43,7 +43,7 @@ const replaceHrefFooter = () => {
     `
 
     links.forEach((link) => {
-      link.setAttribute('href', 'https://test.pupilica.com');
+      link.setAttribute('href', 'https://pupilica.com');
     });
 
   }, []);
