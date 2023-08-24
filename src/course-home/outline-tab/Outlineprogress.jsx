bbf5d2a3ff85eac51dbc9938512dd8ae515c153a
crtmneric <s3rkanodemir@gmail.com>
2023-08-24 15:53:21 +0300

    Changes styles

--- src/course-home/outline-tab/Outlineprogress.jsx
+++ src/course-home/outline-tab/Outlineprogress.jsx
@@ -11,17 +11,17 @@ function OutlineProgress({ courseId }) {
             try {
                 // Define the URL with the actual courseId
                 const url = `https://courses-test.pupilica.com/api/course_home/progress/${courseId}`;
-                
+
                 // Make the GET request using an asynchronous function
                 const response = await getAuthenticatedHttpClient().get(url);
-                
+
                 // Set the fetched data in state
                 setProgressData(response.data.completion_summary);
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
         }
-    
+
         // Call the async function
         fetchData();
     }, [courseId]);
@@ -33,6 +33,11 @@ function OutlineProgress({ courseId }) {
     const lockedCount = progressData?.locked_count || 0;
     const numTotalUnits = completeCount + incompleteCount + lockedCount;
     const completePercentage = completeCount ? Number(((completeCount / numTotalUnits) * 100).toFixed(0)) : 0;
+    const progressBarStyle = {
+        width: `${completePercentage}%`,
+        height: '20px',
+        backgroundColor: 'green',
+    };
 
     // Render the component once completionSummary is available
     return (
@@ -42,6 +47,8 @@ function OutlineProgress({ courseId }) {
             <p>Locked Count: {lockedCount}</p>
             <p>Percentage: {completePercentage}</p>
             {/* Add your JSX code here that uses completionSummary */}
+            {/* Render the progress bar */}
+            <div style={progressBarStyle}></div>
         </div>
     );
 }
