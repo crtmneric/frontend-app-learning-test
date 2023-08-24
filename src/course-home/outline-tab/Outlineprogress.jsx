d1ec2b62a097d23fdf3da2d929672c9f87d39b47
crtmneric <s3rkanodemir@gmail.com>
2023-08-24 14:01:27 +0300

    Changes styles

--- src/course-home/outline-tab/Outlineprogress.jsx
+++ src/course-home/outline-tab/Outlineprogress.jsx
@@ -3,46 +3,35 @@ import { useModel } from '../../generic/model-store';
 
 // Define your new component
 function OutlineProgress({ courseId }) {
-    const [progressData, setProgressData] = useState(null);
+    const { courseId } = useSelector(state => state.courseHome);
 
     useEffect(() => {
-        // Define the URL with the actual courseId
-        const url = `https://courses-test.pupilica.com/api/course_home/progress/${courseId}`;
+        if (courseId) {
+            const response = useModel('progress', courseId);
 
-        // Make the GET request
-        fetch(url)
-            .then((response) => {
-                // Check if the response is successful (status code 200)
-                if (!response.ok) {
-                    throw new Error(`HTTP Error! Status: ${response.status}`);
-                }
-                // Parse the response as JSON
-                return response.json();
-            })
-            .then((data) => {
-                // Set the fetched data in state
-                setProgressData(data.completion_summary);
-            })
-            .catch((error) => {
-                console.error('Error fetching data:', error);
-            });
-    }, [courseId]);
+            console.log("response", response);
+            const completionSummary = response.completion_summary;
+
+            const numTotalUnits = completionSummary.complete_count + completionSummary.incomplete_count + completionSummary.locked_count;
+            const completePercentage = completionSummary.complete_count ? Number(((completionSummary.complete_count / numTotalUnits) * 100).toFixed(0)) : 0;
+            const lockedPercentage = completionSummary.locked_count ? Number(((completionSummary.locked_count / numTotalUnits) * 100).toFixed(0)) : 0;
+
+            console.log("completePercentage", completePercentage);
+            console.log("lockedPercentage ", lockedPercentage );
+        }
+
+    }, [courseId])
 
 
-    // Check if progressData is defined before accessing its properties
-    const completeCount = progressData?.completeCount || 0;
-    const incompleteCount = progressData?.incompleteCount || 0;
-    const lockedCount = progressData?.lockedCount || 0;
-    const numTotalUnits = completeCount + incompleteCount + lockedCount;
-    const completePercentage = completeCount ? Number(((completeCount / numTotalUnits) * 100).toFixed(0)) : 0;
+
 
     // Render the component once progressData is available
     return (
         <div>
-            <p>Complete Count: {completeCount}</p>
+            {/* <p>Complete Count: {completeCount}</p>
             <p>Incomplete Count: {incompleteCount}</p>
-            <p>Locked Count: {lockedCount}</p>
-            <p>Percentage: {completePercentage}</p>
+            <p>Locked Count: {lockedCount}</p> */}
+            {/* <p>Percentage: {completePercentage}</p> */}
             {/* Add your JSX code here that uses progressData */}
         </div>
     );
