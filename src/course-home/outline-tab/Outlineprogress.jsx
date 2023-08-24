5aa22559a744aa34ef5d9f56adf5d52d7d001d79
crtmneric <s3rkanodemir@gmail.com>
2023-08-24 15:33:13 +0300

    Changes styles

--- src/course-home/outline-tab/Outlineprogress.jsx
+++ src/course-home/outline-tab/Outlineprogress.jsx
@@ -1,36 +1,47 @@
 import React, { useEffect, useState } from 'react';
 import { useModel } from '../../generic/model-store';
+import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
 
 // Define your new component
 function OutlineProgress({ courseId }) {
+    const [progressData, setProgressData] = useState(null);
+
     useEffect(() => {
-        if (courseId) {
-            const response = useModel('progress', courseId);
-
-            console.log("response", response);
-            const completionSummary = response.completion_summary;
-
-            const numTotalUnits = completionSummary.complete_count + completionSummary.incomplete_count + completionSummary.locked_count;
-            const completePercentage = completionSummary.complete_count ? Number(((completionSummary.complete_count / numTotalUnits) * 100).toFixed(0)) : 0;
-            const lockedPercentage = completionSummary.locked_count ? Number(((completionSummary.locked_count / numTotalUnits) * 100).toFixed(0)) : 0;
-
-            console.log("completePercentage", completePercentage);
-            console.log("lockedPercentage ", lockedPercentage );
+        async function fetchData() {
+            try {
+                // Define the URL with the actual courseId
+                const url = `https://courses-test.pupilica.com/api/course_home/progress/${courseId}`;
+                
+                // Make the GET request using an asynchronous function
+                const response = await getAuthenticatedHttpClient().get(url);
+                
+                // Set the fetched data in state
+                setProgressData(response.data.completion_summary);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
         }
+    
+        // Call the async function
+        fetchData();
+    }, [courseId]);
 
-    }, [courseId])
 
+    // Check if completionSummary is defined before accessing its properties
+    const completeCount = progressData?.complete_count || 0;
+    const incompleteCount = progressData?.incomplete_count || 0;
+    const lockedCount = progressData?.locked_count || 0;
+    const numTotalUnits = completeCount + incompleteCount + lockedCount;
+    const completePercentage = completeCount ? Number(((completeCount / numTotalUnits) * 100).toFixed(0)) : 0;
 
-
-
-    // Render the component once progressData is available
+    // Render the component once completionSummary is available
     return (
         <div>
-            {/* <p>Complete Count: {completeCount}</p>
+            <p>Complete Count: {completeCount}</p>
             <p>Incomplete Count: {incompleteCount}</p>
-            <p>Locked Count: {lockedCount}</p> */}
-            {/* <p>Percentage: {completePercentage}</p> */}
-            {/* Add your JSX code here that uses progressData */}
+            <p>Locked Count: {lockedCount}</p>
+            <p>Percentage: {completePercentage}</p>
+            {/* Add your JSX code here that uses completionSummary */}
         </div>
     );
 }
