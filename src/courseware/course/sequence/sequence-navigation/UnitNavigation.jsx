45e71f69751dc710b7022df9c3f78ad6fc1eb2c8
crtmneric <s3rkanodemir@gmail.com>
2023-09-14 09:46:18 +0300

    Changes styles

--- src/courseware/course/sequence/sequence-navigation/UnitNavigation.jsx
+++ src/courseware/course/sequence/sequence-navigation/UnitNavigation.jsx
@@ -34,7 +34,7 @@ function UnitNavigation({
       try {
         if (isLastUnit && !progressDataFetched) {
           // Define the URL with the actual courseId
-          const url = `https://courses.pupilica.com/api/course_home/progress/${courseId}`;
+          const url = `https://courses-test.pupilica.com/api/course_home/progress/${courseId}`;
 
           // Make the GET request using an asynchronous function
           const response = await getAuthenticatedHttpClient().get(url);
