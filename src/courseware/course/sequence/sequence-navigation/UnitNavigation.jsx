b408084a936b8c7b4fd07b116e1b4fc761f6fb1f
crtmneric <s3rkanodemir@gmail.com>
2023-09-14 09:44:26 +0300

    Changes styles

--- src/courseware/course/sequence/sequence-navigation/UnitNavigation.jsx
+++ src/courseware/course/sequence/sequence-navigation/UnitNavigation.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 import PropTypes from 'prop-types';
 import { Button } from '@edx/paragon';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
